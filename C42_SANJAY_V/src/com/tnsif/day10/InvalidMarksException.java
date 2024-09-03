8a11b750929bdd11c7dadcf8ccebb6a79bf9cb0c
sanja <sanja@Sanjay_AD>
2024-09-03 20:51:45 +0530

    commit15

---
+++ C42_SANJAY_V/src/com/tnsif/day10/InvalidMarksException.java
@@ -0,0 +1,12 @@
+package com.tnsif.day10;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+}
