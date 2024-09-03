cb9bf5d1a8e0247387ffb100877ac654220c4ff9
sanja <sanja@Sanjay_AD>
2024-09-03 17:40:11 +0530

    commit9

---
+++ C42_SANJAY_V/src/com/tnsif/day6/singleinheritance.java
@@ -0,0 +1,10 @@
+package com.tnsif.day6;
+
+public class singleinheritance {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student("Gayatri", "8080808080", "Bangalore", 9888878787L, 10, "City College");
+		System.out.println(student);
+	}
+}
