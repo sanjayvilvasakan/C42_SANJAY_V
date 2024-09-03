c0a770eeb8729f890d0047c5aeb48e962ce4f9db
sanja <sanja@Sanjay_AD>
2024-09-03 18:30:44 +0530

    commit13

---
+++ C42_SANJAY_V/src/com/tnsif/day8/EmployeeDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.day8;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		
+		Employee e = new Employee("Shubh", 123);
+		System.out.println(e);
+		
+		
+		e = new Employee("Deep", 321);
+		System.out.println(e);
+	}
+}
