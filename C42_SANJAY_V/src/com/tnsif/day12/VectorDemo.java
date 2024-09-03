e0efc1f0c44aea4a994d13578233a55392a9937f
sanja <sanja@Sanjay_AD>
2024-09-03 21:29:26 +0530

    commit16

---
+++ C42_SANJAY_V/src/com/tnsif/day12/VectorDemo.java
@@ -0,0 +1,31 @@
+package com.tnsif.day12;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		Vector<String> animals = new Vector<>();
+		animals.add("Dog");
+		animals.add("Horse");
+		animals.add("Cat");)
+		System.out.println("Initial Vector: " + animals);
+		String element = animals.get(2);
+		System.out.println("Element at index 2: " + element);
+
+		Iterator<String> iterate = animals.iterator();
+		System.out.print("Vector: ");
+		while (iterate.hasNext()) {
+			System.out.print(iterate.next());
+			System.out.print(", ");
+		}
+		element = animals.remove(1);
+		System.out.println("Removed Element: " + element);
+		System.out.println("New Vector: " + animals);
+
+		animals.clear();
+		System.out.println("Vector after clear(): " + animals);
+	}
+
+}
